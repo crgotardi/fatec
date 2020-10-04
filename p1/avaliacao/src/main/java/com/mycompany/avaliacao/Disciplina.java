a7f0cd944859d68f1464edf84e20bf9d033fd307
Cristiano Gotardi <crgotardi@gmail.com>
2020-10-04 18:42:51 -0300

    criação de classe Disciplina

---
+++ p1/avaliacao/src/main/java/com/mycompany/avaliacao/Disciplina.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.avaliacao;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Cristiano
+ */
+public class Disciplina {
+    private String nome;
+    private String ementa;
+    private int ciclo;
+    private float nota;
+    private static final ArrayList<Disciplina> disciplinas = new ArrayList<>();
+    
+    public Disciplina(String nome, String ementa, int ciclo) {
+        this.nome = nome;
+        this.ementa = ementa;
+        this.ciclo = ciclo;
+    }
+    
+    public static ArrayList<Disciplina> getList() {
+        return disciplinas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+}
